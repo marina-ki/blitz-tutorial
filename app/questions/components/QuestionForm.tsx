491b902f1d8774e2a88535bcf6ce752badfbd894
k-810 <k810marina@gmail.com>
2020-11-28 19:09:53 +0900

    fix form

--- app/questions/components/QuestionForm.tsx
+++ app/questions/components/QuestionForm.tsx
@@ -13,7 +13,10 @@ const QuestionForm = ({ initialValues, onSubmit }: QuestionFormProps) => {
         onSubmit(event)
       }}
     >
-      <div>Put your form fields here. But for now, just click submit</div>
+      <input placeholder="Name" />
+      <input placeholder="Choice 1" />
+      <input placeholder="Choice 2" />
+      <input placeholder="Choice 3" />
       <div>{JSON.stringify(initialValues)}</div>
       <button>Submit</button>
     </form>
--- app/questions/pages/questions/new.tsx
+++ app/questions/pages/questions/new.tsx
@@ -13,19 +13,16 @@ const NewQuestionPage: BlitzPage = () => {
 
       <QuestionForm
         initialValues={{}}
-        onSubmit={async () => {
+        onSubmit={async (event) => {
           try {
             const question = await createQuestionMutation({
               data: {
-                text: "MyName",
+                text: event.target[0].value,
                 choices: {
                   create: [
-                    {
-                      text: "Yes",
-                    },
-                    {
-                      text: "No",
-                    },
+                    { text: event.target[1].value },
+                    { text: event.target[2].value },
+                    { text: event.target[3].value },
                   ],
                 },
               },
